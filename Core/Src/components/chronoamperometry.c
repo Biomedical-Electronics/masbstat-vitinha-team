ffe14b713de675cb06795dd1c99f3ed251b3cac8
Elliot Gonzalez <elgo3000@gmail.com>
2022-05-10 14:43:46 +0200

    chrono con timer

--- Core/Src/components/chronoamperometry.c
+++ Core/Src/components/chronoamperometry.c
@@ -10,22 +10,27 @@
 uint32_t VrefADC;
 uint32_t IADC; // adc value from Icell
 double Icell;
+extern &htim2;
 
 void ChronoAmperometry(struct CA_Configuration_S caConfiguration){
+
 
 	double OutV=caConfiguration.eDC;
 	uint32_t periodms=caConfiguration.samplingPeriodMs;
 	uint32_t duration=caConfiguration.measurementTime;
 	uint16_t n_samples=duration/periodms;
 	struct Data_S sendPackage;
-
 	MCP4725_SetOutputVoltage(hdac,calculateDacOutputVoltage(OutV)); // NUEVA TENSION
+	TIM_TimeBaseInitStructure.TIM_Period = period - 1;
+	TIM_TimeBaseInitStructure.TIM_Prescaler = 8399+1;
+	HAL_TIM_Base_Start_IT(&htim2);
 	HAL_ADC_Start(&hadc1); // iniciamos la conversion para el divisor de tension
 	HAL_ADC_PollForConversion(&hadc1, 200); // esperamos que finalice la conversion
 	    							// si en 200 ms no termina, salimos
 	VrefADC = HAL_ADC_GetValue(&hadc1);
-
-	for (i=0;i<n_samples;i++){
+	int i=0;
+	while (i<n_samples) {
+		if (estadoMEDIDA==1){
 		HAL_ADC_Start(&hadc1); // iniciamos la conversion para la intensidad de la celda
 		HAL_ADC_PollForConversion(&hadc1, 200); // esperamos que finalice la conversion
 																							// si en 200 ms no termina, salimos
@@ -39,9 +44,16 @@ void ChronoAmperometry(struct CA_Configuration_S caConfiguration){
 		sendPackage.point = i;
 
 		MASB_COMM_S_sendData(sendPackage);
-
+		i++;
+		estadoMEDIDA=0;
+		}
 	}
 }
 
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+
+	estadoMEDIDA = 1;
+
+}
 
 
